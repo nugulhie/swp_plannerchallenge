af5c497cf5ff3bc6d5f4dd36d103b5dc5e603ba3
cheongeun <svosu0175@gmail.com>
2021-03-28 23:14:04 +0900

    layout 0328 edit / edit layout

--- app/src/main/java/com/example/swp_challenge/MainActivity.java
+++ app/src/main/java/com/example/swp_challenge/MainActivity.java
@@ -1,35 +1,74 @@
 package com.example.swp_challenge;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-
-import java.util.Date;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText text_curAchi = findViewById(R.id.text_curAchi_main);
-    private Button button_Addchall = findViewById(R.id.button_Addchall_main);
-    private Button button_detail = findViewById(R.id.button_detail_main);
-    private Button img_cal = findViewById(R.id.img_cal_main);
-    private Button img_setting = findViewById(R.id.img_setting_main);
-    private Button img_box = findViewById(R.id.img_box_main);
-    View text_Curdate = findViewById(R.id.text_Curdate_main);
-    ProgressBar progressBar_reward = findViewById(R.id.progressBar_reward_main);
-    RecyclerView recyclerView_Plan = findViewById(R.id.recycler_plan_main);
-    RecyclerView recyclerView_Chall = findViewById(R.id.recycler_Chall_main);
+//    TextView text_curAchi = findViewById(R.id.text_curAchi_main);
+    Button button_Add_challenge;
+    Button button_detail;
+    Button img_cal;
+    Button img_setting;
+    Button img_box;
+  //  TextView text_Curdate = findViewById(R.id.text_Curdate_main);
+ //   ProgressBar progressBar_reward = findViewById(R.id.progressBar_reward_main);
+  //  RecyclerView recyclerView_Plan = findViewById(R.id.recycler_plan_main);
+  //  RecyclerView recyclerView_Chall = findViewById(R.id.recycler_Chall_main);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        button_Add_challenge = findViewById(R.id.button_Addchall_main);
+        button_detail = findViewById(R.id.button_detail_main);
+        img_cal = findViewById(R.id.img_cal_main);
+        img_setting = findViewById(R.id.img_setting_main);
+        img_box = findViewById(R.id.img_box_main);
 
+        button_Add_challenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder add_challenge = new AlertDialog.Builder(MainActivity.this);
+                add_challenge.setIcon(R.mipmap.ic_launcher);
+                add_challenge.setTitle("제목");
+                add_challenge.setMessage("testing");
+                final EditText challengeName = new EditText(MainActivity.this);
+                final EditText challengeContents = new EditText(MainActivity.this);
+                final EditText challengeDate = new EditText(MainActivity.this);
+                RatingBar challengeRate = new RatingBar(MainActivity.this);
+                add_challenge.setPositiveButton("추가하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String challname = challengeName.getText().toString();
+                        String challcontents = challengeContents.getText().toString();
+                        String challdate = challengeDate.getText().toString();
+                        dialog.dismiss();
+                    }
+                });
+                add_challenge.setPositiveButton("삭제하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+//이 밑으로 전부 intent 함수
         button_detail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/swp_challenge/SettingsActivity.java
+++ app/src/main/java/com/example/swp_challenge/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.swp_challenge;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.settings_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager()
